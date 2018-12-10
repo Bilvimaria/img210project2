d82352329a2961b8dc98f028fdb08cd65ee47176
Bilvi Maria Joseph <bilvimariajoseph@Bilvis-MBP.hitronhub.home>
2018-12-09 18:17:09 -0800

    adds custom side bar

---
+++ wp-content/themes/twentyseventeen-child/template-parts/page/content-page.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+
+
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php 
+		// the_title( '<h1 class="entry-title">', '</h1>' );
+		 ?>
+		<?php 
+		get_sidebar(); 
+		// twentyseventeen_edit_link( get_the_ID() );
+		 ?>
+			<!-- my edits -->
+		<?php 
+		// get_sidebar(); 
+		?>
+			<!-- my edits ends -->
+	</header><!-- .entry-header -->
+	
+	<div class="entry-content">
+		<?php
+			the_content();
+		
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'twentyseventeen' ),
+				'after'  => '</div>',
+			) );
+		?>
+
+		<!-- my edits -->
+		<div class="my-sidebar">
+		hello
+		<?php 
+		// get_sidebar(); 
+		?>
+		</div>
+			<!-- my edits end -->
+		
+	</div><!-- .entry-content -->
+
+</article><!-- #post-## -->
